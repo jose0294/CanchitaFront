1437eeee85002c0e3ff925d7a885990d4765e27f
carlosclabsis <carlosclabsis@gmail.com>
2019-01-22 17:11:09 -0500

    Creacion del componente Local
    Cree el componente local, de acuerdo a la base de datos, para realizar su respectiva administracion.

--- canchita/src/app/app.module.ts
+++ canchita/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LocalComponent } from './local/local.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LocalComponent
   ],
   imports: [
     BrowserModule
